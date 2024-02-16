554c3ab62434ea25d6166b3b32204e1d4f4a5b6c
David Planchon <davidplanchon.68@gmail.com>
2024-02-17 00:06:45 +0100

    setup token access

--- start/routes.ts
+++ start/routes.ts
@@ -1,13 +1,14 @@
-/*
-|--------------------------------------------------------------------------
-| Routes file
-|--------------------------------------------------------------------------
-|
-| The routes file is used for defining the HTTP routes.
-|
-*/
-
 const UsersController = () => import('#controllers/users_controller')
 import router from '@adonisjs/core/services/router'
+import User from '#models/user'
 
 router.get('/', [UsersController, 'index'])
+router.post('users/:id/tokens', async ({ params }) => {
+  const user = await User.findOrFail(params.id)
+  const token = await User.accessTokens.create(user)
+
+  return {
+    type: 'bearer',
+    value: token.value!.release(),
+  }
+})
