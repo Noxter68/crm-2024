df27499810b8a426495c06a54f9368b0d0a09088
David Planchon <davidplanchon.68@gmail.com>
2024-02-18 17:23:09 +0100

    Setting up Auth

--- app/controllers/users_controller.ts
+++ app/controllers/users_controller.ts
@@ -1,11 +1,10 @@
-// import type { HttpContext } from '@adonisjs/core/http'
 
 import User from '#models/user'
 import { HttpContext } from '@adonisjs/core/http'
 
 export default class UsersController {
   async index({}: HttpContext) {
-    const users = await User.findBy('id', 2)
+    const users = await User.findBy('id', 1)
 
     return users
   }
