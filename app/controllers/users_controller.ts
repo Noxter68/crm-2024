4752e65fadd7edc9dde4a42e983c6bebacec40d3
David Planchon <davidplanchon.68@gmail.com>
2024-02-23 23:04:23 +0100

    add prettier & change sessions token to api token

--- app/controllers/users_controller.ts
+++ app/controllers/users_controller.ts
@@ -1,4 +1,3 @@
-
 import User from '#models/user'
 import { HttpContext } from '@adonisjs/core/http'
 
--- start/routes.ts
+++ start/routes.ts
@@ -1,10 +1,13 @@
 const UsersController = () => import('#controllers/users_controller')
 import router from '@adonisjs/core/services/router'
 import User from '#models/user'
+import { middleware } from './kernel.js'
 
-router.get('/', [UsersController, 'index'])
+router.get('/', [UsersController, 'index']).use([middleware.auth()])
+
 router.post('users/:id/tokens', async ({ params }) => {
   const user = await User.findOrFail(params.id)
+
   const token = await User.accessTokens.create(user)
 
   return {
